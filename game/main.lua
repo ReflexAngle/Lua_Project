4ba58b064540973c297f4efeddf216d1087df1b1
Valentine <valnkowa@gmail.com>
2025-03-03 08:28:16 -0600

    Organized files & folders
    Organized files & folders

--- game/main.lua
+++ game/main.lua
@@ -1,56 +1,57 @@
-local major, minor, revision, codename = love.getVersion()
+local major, minor, revision = love.getVersion()
 local screenWidth = love.graphics.getWidth() -- Gets the screen width
 local screenHeight = love.graphics.getHeight() -- Gets the screen height
 
-local anim8 = require("anim8") -- anim8 library for animations
-
+--local anim8 = require('libs.anim8') -- anim8 library for animations
+local player = require("src.player") -- player class
 
 -- setting sprites
 local sprites = {
-    background = love.graphics.newImage("background.png"),
-    player = love.graphics.newImage("Player.png"),
-    playerWalkSheet = love.graphics.newImage("playerWalkSheet.png")
+    background = love.graphics.newImage("assets/imgs/background.png"),
+    --player = love.graphics.newImage("Player.png"),
+    --playerWalkSheet = love.graphics.newImage("playerWalkSheet.png")
 }
 
+-- scaling and sizing
 local bgWidth = sprites.background:getWidth()
 local bgHeight = sprites.background:getHeight()
-local frameWidth = 16
-local frameHeight = 32
-
--- grid for anim8 to use to pick for animations
-local grid = anim8.newGrid(frameWidth, frameHeight, sprites.playerWalkSheet:getWidth(), sprites.playerWalkSheet:getHeight()) -- Creates a grid for the player sprite
-
-
--- scaling and sizing
-local playerScaler = 3 -- scales the player sprite by 3
 local scale = math.min(screenWidth / bgWidth, screenHeight / bgHeight)
 local scaledWidth = bgWidth * scale
 local scaledHeight = bgHeight * scale
 local offsetX = (screenWidth - scaledWidth) / 2
 local offsetY = (screenHeight - scaledHeight) / 2
 
--- main player config
-local player =
-{
-    dragging = false,
-    pressed = false,
-    walkSpeed = 3,
-    transform = {
-        x = (screenWidth - frameWidth * playerScaler) / 2,
-        y = (screenHeight - frameHeight * playerScaler) / 2,
-        width = frameWidth * playerScaler,
-        height = frameHeight * playerScaler
-    },
+local frameWidth = 16
+local frameHeight = 32
+local playerScaler = 3 -- scales player sprite
 
-    animations = {
-        idle = anim8.newAnimation(grid('1-1', 1), 0.1),
-        walkDown = anim8.newAnimation(grid('1-4', 1), 0.1),
-        walkRight = anim8.newAnimation(grid('2-4', 2), 0.1),
-        walkUp = anim8.newAnimation(grid('3-4', 3), 0.1),
-        walkLeft = anim8.newAnimation(grid('4-4', 4), 0.1),
-        currWalk = anim8.newAnimation(grid('1-4', 1), 0.1),
-    }
-}
+-- grid for anim8 to use to pick for animations
+--local grid = anim8.newGrid(frameWidth, frameHeight, sprites.playerWalkSheet:getWidth(), sprites.playerWalkSheet:getHeight()) -- Creates a grid for the player sprite
+
+
+
+-- main player config
+-- local player =
+-- {
+--     dragging = false,
+--     pressed = false,
+--     walkSpeed = 3,
+--     transform = {
+--         x = (screenWidth - frameWidth * playerScaler) / 2,
+--         y = (screenHeight - frameHeight * playerScaler) / 2,
+--         width = frameWidth * playerScaler,
+--         height = frameHeight * playerScaler
+--     },
+
+--     animations = {
+--         idle = anim8.newAnimation(grid('1-1', 1), 0.1),
+--         walkDown = anim8.newAnimation(grid('1-4', 1), 0.1),
+--         walkRight = anim8.newAnimation(grid('2-4', 2), 0.1),
+--         walkUp = anim8.newAnimation(grid('3-4', 3), 0.1),
+--         walkLeft = anim8.newAnimation(grid('4-4', 4), 0.1),
+--         currWalk = anim8.newAnimation(grid('1-4', 1), 0.1),
+--     }
+-- }
 
 
 
@@ -58,16 +59,19 @@ function love.load() -- This runs once at the start of the game
     -- fonts
     local font = love.graphics.newFont(16)
     love.graphics.setFont(font)
+    player:load(screenWidth, screenHeight, frameWidth, frameHeight, playerScaler)
 end
 
 function love.update(dt)
+    local pressed = false
+
     --mouse movement for dragging
     if  player.dragging then
         player.transform.x = love.mouse.getX() - player.transform.width / 2
         player.transform.y = love.mouse.getY() - player.transform.height / 2
     end
 
-    --movement
+    --keyboard movement
     if love.keyboard.isDown("right") or love.keyboard.isDown("d") then
         player.transform.x = player.transform.x + player.walkSpeed
         player.animations.currWalk = player.animations.walkRight
@@ -95,7 +99,7 @@ function love.update(dt)
     if not pressed then
         player.animations.currWalk = player.animations.idle
     end
-    player.animations.currWalk:update(dt) -- updates the current animation
+    player:update(dt) -- updates the current animation
 end
 
 function love.draw() -- draws graphics
@@ -105,15 +109,16 @@ function love.draw() -- draws graphics
  
     -- drawing background 
     love.graphics.draw(sprites.background, offsetX, offsetY, 0, scale, scale) 
-    
+    -- draw player
+    player:draw(offsetX, offsetY, scaledWidth, scaledHeight)
     -- testing walk animations
-    player.animations.currWalk:draw(
-    sprites.playerWalkSheet,
-    offsetX + player.transform.x,
-    offsetY + player.transform.y,
-    0,
-    playerScaler,
-    playerScaler)
+    -- player.animations.currWalk:draw(
+    -- sprites.playerWalkSheet,
+    -- offsetX + player.transform.x,
+    -- offsetY + player.transform.y,
+    -- 0,
+    -- playerScaler,
+    -- playerScaler)
 end
 
 
@@ -124,8 +129,7 @@ function love.mousepressed(x, y)
     if x > player.transform.x
         and x < player.transform.x + player.transform.width
         and y > player.transform.y
-        and y < player.transform.y + player.transform.height
-    then
+        and y < player.transform.y + player.transform.height then
         player.dragging = true
     end
 end
---
+++ game/src/player.lua
@@ -0,0 +1,61 @@
+local anim8 = require 'libs/anim8'
+
+local Player = {}
+
+function Player:load(screenWidth, screenHeight, frameWidth, frameHeight, playerScaler)
+    self.sprites = {
+        playerWalkSheet = love.graphics.newImage('assets/imgs/playerWalkSheet.png')
+    }
+    self.walkSpeed = 3
+    self.playerScaler = 3
+    self.dragging = false
+    self.pressed = false
+    self.transform = {
+        x = (screenWidth - frameWidth * playerScaler) / 2,
+        y = (screenHeight - frameHeight * playerScaler) / 2,
+        width = frameWidth * playerScaler,
+        height = frameHeight * playerScaler
+    }
+
+    -- load player sprites
+    
+    -- grid for anim8
+    local grid = anim8.newGrid(
+    frameWidth,
+    frameHeight,
+    self.sprites.playerWalkSheet:getWidth(),
+    self.sprites.playerWalkSheet:getHeight())
+
+    -- player animations
+    self.animations = {
+        idle = anim8.newAnimation(grid('1-1', 1), 0.1),
+        walkDown = anim8.newAnimation(grid('1-4', 1), 0.1),
+        walkRight = anim8.newAnimation(grid('2-4', 2), 0.1),
+        walkUp = anim8.newAnimation(grid('3-4', 3), 0.1),
+        walkLeft = anim8.newAnimation(grid('4-4', 4), 0.1),
+        currWalk = anim8.newAnimation(grid('1-4', 1), 0.1),
+    }
+    --default animation
+    self.animations.currWalk = self.animations.idle
+end
+
+
+
+function Player:update(dt)
+    self.animations.currWalk:update(dt)    
+end
+
+
+
+
+function Player:draw(offsetX, offsetY, scaledWidth, scaledHeight)
+    self.animations.currWalk:draw(self.sprites.playerWalkSheet,
+    self.transform.x,
+    self.transform.y,
+    0,
+    self.playerScaler,
+    self.playerScaler)
+    
+end
+
+return Player
