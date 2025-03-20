bc94a3b6333b81c16aea06c25a4cfb6fed629bd8
Valentine <valnkowa@gmail.com>
2025-03-20 13:40:49 -0500

    added in sword image
    + sword

--- game/src/slash.lua
+++ game/src/slash.lua
@@ -5,7 +5,8 @@ local Slash = {}
 
 function Slash:load()
     self.image = love.graphics.newImage('assets/imgs/Slashhh.png')
-  
+    self.swordImg = love.graphics.newImage('assets/imgs/Sword.png')
+
     local imgWidth = self.image:getWidth()
     local imgHeight = self.image:getHeight()
     local frameWidth = imgWidth / 3
@@ -21,6 +22,8 @@ function Slash:load()
     
     self.animation = anim8.newAnimation(grid('1-3', 1), 0.1) -- Adjust based on your frames
     self.active = false
+    self.swordActive = false -- tracks sword
+    self.swordX, self.swordY = 0, 0 -- sword position
     self.x, self.y = 0, 0
     self.angle = 0
 end
@@ -28,6 +31,8 @@ end
 function Slash:activate(player)
     self.x, self.y = player.transform.x + player.transform.width / 2, player.transform.y + player.transform.height / 2
     self.active = true
+    self.swordActive = true
+    --self.swordX, self.swordY = player.transform.x + player.transform.width / 2, player.transform.y + player.transform.height / 2
     self.scale = player.playerScaler * 1  -- Adjust scale proportionally
     --self.animation:gotoFrame(1)  -- Reset animation
 
@@ -36,15 +41,23 @@ function Slash:activate(player)
     if player.direction == "right" then
         self.angle = 0
         self.x = self.x + 15 + offset
+        self.swordX = self.x
+        self.swordY = self.y
     elseif player.direction == "left" then
         self.angle = math.pi  -- 180 degrees (flip)
         self.x = self.x - 15 - offset
+        self.swordX = self.x
+        self.swordY = self.y
     elseif player.direction == "up" then
         self.angle = -math.pi / 2  -- 90 degrees counterclockwise
         self.y = self.y - 15 - offset
+        self.swordX = self.x
+        self.swordY = self.y - 10 - offset
     elseif player.direction == "down" then
         self.angle = math.pi / 2  -- 90 degrees clockwise
         self.y = self.y + 15 + offset
+        self.swordX = self.x
+        self.swordY = self.y + 10 + offset
     end
 end
 
@@ -52,10 +65,11 @@ end
 function Slash:update(dt)
     if self.active then
        self.animation:update(dt)
-    end
-
-    if self.animation.position == #self.animation.frames then
-        self.active = false  -- Disable the slash after one cycle
+       
+        if self.animation.position == #self.animation.frames then
+            self.active = false  -- Disable the slash after one cycle
+            self.swordActive = false
+        end
     end
 end
 
@@ -70,6 +84,11 @@ function Slash:draw()
             self.image:getWidth() / 4,
             self.image:getHeight() / 2 -- Origin point
         )
+    end
+
+    -- draws swordimg
+    if self.swordActive then
+        love.graphics.draw(self.swordImg, self.swordX, self.swordY, self.angle - 90, 1, 1, self.swordImg:getWidth() / 3, self.swordImg:getHeight() / 3)
     end
 end
 
